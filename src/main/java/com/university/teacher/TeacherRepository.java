dd1c59c1954fc681141e4d1700f9545f2a1be88c
ppolanin <polaninp@gmail.com>
2022-01-21 15:00:16 +0100

    Teacher repository enables to find lower case inputs

--- src/main/java/com/university/teacher/TeacherRepository.java
+++ src/main/java/com/university/teacher/TeacherRepository.java
@@ -10,13 +10,13 @@ import java.util.List;
 @Repository
 public interface TeacherRepository extends GenericRepository<Teacher> {
 
-    @Query("SELECT t FROM Teacher t WHERE t.firstName = :firstName")
+    @Query("SELECT t FROM Teacher t WHERE lower(t.firstName) = lower(:firstName)")
     List<Teacher> findByFirstName(@Param("firstName") String firstName);
 
-    @Query("SELECT t FROM Teacher t WHERE t.lastName = :lastName")
+    @Query("SELECT t FROM Teacher t WHERE lower(t.lastName) = lower(:lastName)")
     List<Teacher> findByLastName(@Param("lastName") String lastName);
 
-    @Query("SELECT t FROM Teacher t WHERE t.firstName = :firstName AND t.lastName = :lastName")
+    @Query("SELECT t FROM Teacher t WHERE lower(t.firstName) = lower(:firstName) AND lower(t.lastName) = lower(:lastName)")
     List<Teacher> findByFullName(@Param("firstName") String firstName, @Param("lastName") String lastName);
 
 //    @Query(nativeQuery = true, value = "SELECT * FROM teachers t INNER JOIN students_teachers st ON st.teacher_id = t.id WHERE st.student_id = :studentId")
